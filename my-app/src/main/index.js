e9b4eb2e095367e9db7f2986db182b873ff47926
Mariam0422 <mariam79.yan@gmail.com>
2024-08-20 00:31:39 +0400

    add basic logic

--- my-app/src/main/index.js
+++ my-app/src/main/index.js
@@ -1,19 +1,27 @@
 import { useState, useEffect } from 'react';
 import { API_KEY } from '../core/constant';
 import { getRandomElementArr } from '../core/helpers';
+import './index.css';
 const WeatherData =  () => {
     const [weatherTemp, setWeatherTemp] = useState(0);
     const [currentCity, setCurrentCity] = useState("");
-    const cityArray = ["Yerevan", "London", "Sydney", "Moscow", "Madrid", "Cuba", "Italy"];
+    const [answer, setAnswer] = useState(null);
+    const [value, setValue] = useState(0);
+    const [isVisibility, setIsVisibility] = useState(false);
+    const cityArray = ["Yerevan", "London", "Sydney", "Moscow", "Madrid", "Cuba", "Italy"];    
 
-    useEffect(() => {
+    const gameAgain = () => {
       const randomCity = getRandomElementArr(cityArray);
       setCurrentCity(randomCity);
-    }, []); 
-    
+      setValue("");
+      setIsVisibility(false);
+    }
+    useEffect((() => {
+      gameAgain();
+    }), [])
+  
     useEffect(() => {
-      if (currentCity === "") return; 
-      
+      if (currentCity === "") return;       
       const getWeatherData = async () => {
           try{
               const response = await fetch(
@@ -21,7 +29,7 @@ const WeatherData =  () => {
               );
               const data = await response.json();
               console.log(data);
-              const temp = data.main.temp.toFixed(0);
+            const temp = data.main.temp.toFixed(0);
               setWeatherTemp(temp);
           } catch(error) {
               console.log(error);
@@ -29,10 +37,31 @@ const WeatherData =  () => {
       }
       getWeatherData();
     }, [currentCity]); 
-   console.log(currentCity);
+    
+    const handleInput = (e) => {
+     setValue(e.target.value);
+    }
+    const handleCheck = () => {
+      if(Math.abs(weatherTemp - value) <= 4){
+        setAnswer(true)
+      }
+      else{
+        setAnswer(false)
+      }
+     setIsVisibility(true)
+    }
+ 
+   
     return (
+      <div className='main'>
+       <h2> {currentCity} </h2>
+      <input  value={value} onChange={handleInput} type='text'/>
       <div>
-        {weatherTemp}
+      <h3 className='weatherTemp' style={{display: isVisibility ? "block" :  "none"}}>{weatherTemp}</h3>     
+      <button onClick={handleCheck}>Check</button>
+      { isVisibility && (answer ? "right answer" : "wrong answer")} 
+      <button onClick={gameAgain}>Next</button>
+      </div>
       </div>
     )
   }
